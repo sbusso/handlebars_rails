c17c644096d2b49a9c2b6821ce70b7eda8c23616
Stephane Busso <stephane.busso@gmail.com>
2011-11-20 00:54:31 +0700

    register engine at environment level

--- lib/handlebars_assets/engine.rb
+++ lib/handlebars_assets/engine.rb
@@ -1,8 +1,8 @@
 module HandlebarsAssets
   class Engine < ::Rails::Engine
     initializer "sprockets.handlebars", :after => "sprockets.environment", :group => :assets do |app|
-      next unless app.assets
-      app.assets.register_engine('.hbs', TiltHandlebars)
+	  next unless app.assets
+      environment.register_engine('.hbs', TiltHandlebars)
     end
   end
 end
